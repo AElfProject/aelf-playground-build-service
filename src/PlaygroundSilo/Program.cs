375c4e7c419b3513e9d9c5aa0c411deb59d87184
yongen.loong <yongen.loong@aelf.io>
2024-10-21 23:27:30 +0800

    feat: add zookeeper clustering

--- src/PlaygroundSilo/Program.cs
+++ src/PlaygroundSilo/Program.cs
@@ -2,10 +2,21 @@
 using Microsoft.Extensions.Logging;
 
 IHostBuilder builder = Host.CreateDefaultBuilder(args)
-    .UseOrleans(silo =>
+    .UseOrleans((context, silo) =>
     {
-        silo.UseLocalhostClustering()
-            .ConfigureLogging(logging => logging.AddConsole());
+        silo.UseZooKeeperClustering(options =>
+        {
+            // for development, start services using docker compose.
+            options.ConnectionString = Environment.GetEnvironmentVariable("ASPNETCORE_ZOOKEEPER_HOST") ?? "localhost:2181";
+        });
+
+        var POD_NAMESPACE = Environment.GetEnvironmentVariable("POD_NAMESPACE"); // in kubernetes, this will not be empty
+        if (!string.IsNullOrEmpty(POD_NAMESPACE)) // only use kubernetes hosting when running in kubernetes
+        {
+            silo.UseKubernetesHosting();
+        }
+
+        silo.ConfigureLogging(logging => logging.AddConsole());
     })
     .UseConsoleLifetime();
 
